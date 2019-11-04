aba0e4bbe47a32e056972f2ce21e646a56f41c92
Jordi Prats <jordi.prats@gmail.com>
2019-11-04 10:11:36 +0100

    nota

--- gatekeeperdesktop.py
+++ gatekeeperdesktop.py
@@ -98,11 +98,8 @@ class Login(QtWidgets.QDialog):
                 import notify2
 
                 notify2.init("gatekeeper Desktop")
-
                 n = notify2.Notification("gatekeeper Desktop", "service-now Login successfull")
-
                 n.set_urgency(notify2.URGENCY_NORMAL)
-
                 n.show()
 
         except Exception as e:
